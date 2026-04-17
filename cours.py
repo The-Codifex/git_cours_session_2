14d083dc3a2573456af8cbd6d7f747f72dc36bbc
Esteban Ramirez <e2558391@cmaisonneuve.qc.ca>
2026-04-17 11:47:40 -0400

    ajout de courriel dans cours.py

--- cours.py
+++ cours.py
@@ -1,10 +1,11 @@
 from eleve import *
 class courso:
-    def __init__(self, nom, nombre, descripcion,attribut_de_git):
+    def __init__(self, nom, nombre, descripcion,attribut_de_git, courriel):
         self.nom = nom
         self.nombre = nombre
         self.descripcion = descripcion
         self.attribut_de_git = attribut_de_git
+        self.courriel = courriel
 
     def __str__(self):
         return f'{self.nom} {self.nombre} {self.descripcion}'
