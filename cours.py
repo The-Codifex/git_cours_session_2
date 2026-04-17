9eb60196f2cdcbeadd2c89dcd1c59200f01d76d4
Esteban Ramirez <e2558391@cmaisonneuve.qc.ca>
2026-04-17 11:20:32 -0400

    elements de courso mis

---
+++ cours.py
@@ -0,0 +1,10 @@
+class courso:
+    def __init__(self, nom, nombre, descripcion):
+        self.nom = nom
+        self.nombre = nombre
+        self.descripcion = descripcion
+
+
+    def __str__(self):
+        return f'{self.nom} {self.nombre} {self.descripcion}'
+    
