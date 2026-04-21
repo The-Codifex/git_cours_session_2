a48f1fea31500d36f51b8467189e8dccbbad836f
Esteban Ramirez <e2558391@cmaisonneuve.qc.ca>
2026-04-21 16:59:55 -0400

    .

--- cours.py
+++ cours.py
@@ -1,14 +1,15 @@
 from eleve import *
 class courso:
-    def __init__(self, nom, nombre, descripcion,attribut_de_git, courriel):
+    def __init__(self, nom, nombre, descripcion,attribut_de_git, courriel, sigle_cours):
         self.nom = nom
         self.nombre = nombre
         self.descripcion = descripcion
         self.attribut_de_git = attribut_de_git
         self.courriel = courriel
+        self.sigle_cours = sigle_cours
 
     def __str__(self):
-        return f'{self.nom} {self.nombre} {self.descripcion}'
+        return f'{self.nom} {self.nombre} {self.sigle_cours}'
     
 
 i=1
